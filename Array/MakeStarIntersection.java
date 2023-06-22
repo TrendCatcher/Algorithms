0b8f0c5ffe60dc98e6c4f451b816929205f2842a
TrendCatcher <karleo@kyonggi.ac.kr>
2023-06-22 23:27:45 +0900

    Finished the code of MakeStarIntersection.java but need to fixed...

--- Array/MakeStarIntersection.java
+++ Array/MakeStarIntersection.java
@@ -8,8 +8,9 @@ package Array;
 *
 * 입력: 직선 A,B,C에 대한 정보가 담긴 배열 line이 매개변수로 주어짐. 모든 별을 포함하는 최소 사각형을 return하도록
 * 출력: 모든 별을 포함하는 최소 사각형을 return
-*
-* 풀이 흐름
+*/
+
+/* 풀이 흐름
 * 1. 모든 직선 쌍에 대해 반복
 *   1.1. 교점 좌표 구하기
 *   1.2. 정수 좌표만 저장
@@ -20,6 +21,7 @@ package Array;
  * */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MakeStarIntersection {
@@ -30,37 +32,93 @@ public class MakeStarIntersection {
             this.y=y;
         }
     }
-    for(int i=0; i < line.length; i++){     //모든 직선쌍에서 반복
-        for(int j=1; j < line.length; j++ ){
-            // line[i], line[j]를 사용하여 1-A, 1-B 수행
-        }
-    }
+
+
     // 1-A
     private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2){
         //교점 구해서 반환하기
         double x = (double)(b1*c2-b2*c1)/(a1*b2-a2*b1);
         double y = (double)(a2*c1-a1*c2)/(a1*b2-a2*b1);
+
+        if(x%1!=0||y%1!=0) return null;
+
         return null;
     }
 
     //1-B 정수 좌표만 저장
     // 이 메서드를 사용하며 정수 좌표일 때 Point 객체가 반환되므로 좌표를 저장할 리스트를 만들고, 객체가 반환되었을 때만 리스트에 저장
-    List<Point> points = new ArrayList<>();
-    for(int i=0; i<line.length;i++){
-        for(int j= i+1; j<line.length;j++){
-            Point intersection = intersection(line[i][0], line[i][1], line[i][2],
-                    line[j][0], line[j][1], line[j][2]);
-            if(intersection !=null){
-                points.add(intersection);
+
+    // 2. 저장된 정수들에 대해 x,y의 최대최소 구하기
+    private Point getMinimumPoint(List<Point> points) {
+        //가장 작은 좌표 찾기, points 리스트 안의 모든 Point 객체를 순회하면서
+        // 가장 작은 x,y값을 찾고 Point 객체를 만들어 반환
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+
+        for (Point p : points) {
+            if (p.x < x) x = p.x;
+            if (p.y < y) y = p.y;
+        }
+        return new Point(x, y);
+
+    }
+    private Point getMaximumPoint(List<Point> points){
+        //가장 큰 좌표 찾기
+        long x = Long.MIN_VALUE;
+        long y = Long.MIN_VALUE;
+
+        for (Point p : points) {
+            if (p.x > x) x = p.x;
+            if (p.y > y) y = p.y;
+        }
+        return new Point(x, y);
+    }
+    public String[] MakeStarIntersection(int [][]line){
+        List<Point>points=new ArrayList<>();
+        for(int i=0; i < line.length; i++){     //모든 직선쌍에서 반복
+            for(int j=1; j < line.length; j++ ){
+                // line[i], line[j]를 사용하여 1-A, 1-B 수행
+                Point intersection = intersection(line[i][0], line[i][1], line[i][2],
+                        line[j][0], line[j][1], line[j][2]);
+                if(intersection !=null){
+                    points.add(intersection);
             }
         }
     }
-    // 저장된 정수들에 대해 x,y의 최대최소 구하기
+    //3. 구한 최대,최소를 이용하여 2차원 배열의 크기 설정
+    // 배열 크기를 설정해야 하므로 +1에 유의!!
+    Point minimum = getMinimumPoint(points);
+    Point maximum = getMaximumPoint(points);
 
-    public void solution(){
+    int width = (int) (maximum.x- minimum.x+1);
+    int height = (int) (maximum.y- minimum.y+1);
+
+    char[][] arr = new char[height][width];
+    for(char [] row : arr){
+        Arrays.fill(row,'.');
+    }
+
+    //4. 2차원 배열에 표시
+    //별을 찍을 위치인 교점 정보는 points 변수에 있으니 순회하면서 별을 찍음
+    for(Point p : points){
+        //2차원 배열에 별 찍기
+        /*주의점*/
+        // 2차원배열상과 좌표상은 위치는 다르므로 변환해야함
+        int x = (int)(p.x- minimum.x);
+        int y = (int) (maximum.y - p.y);
+        arr[y][x] ='*';
+    }
+
+    //5. 문자열 배열로 변환 후 반환
+    String [] result = new String[arr.length];
+    for(int i =0; i<result.length;i++){
+        result[i]= new String(arr[i]);
+    }
+    return result;
 
     }
-    public static void main(String[]args){
-        new MakeStarIntersection().solution();
+
+    public static void main(String[] args) {
+        new MakeStarIntersection();
     }
 }
--- README.md
+++ README.md
@@ -33,6 +33,7 @@
 | 2023.06.15 | Sort         | 2750(오름차순)                                                                                                                                               | `진행 완료`   |
 | 2023.06.18 | Reading Book | [Programmers Coding Test Problem Solving Strategy] - Java  [1장 Notion 정리](https://www.notion.so/Programmers-Java-0334141765a84fcf8e03793b0c98e3a3?pvs=4) | `진행 완료`   |
 | 2023.06.20 | Array        | Programmers level2(교점에 별 만들기)                                                                                                                            | `진행 예정`   |
+| 2023.06.22 | Array        | Programmers level2(교점에 별 만들기) 코드 완성 및 부분 복습                                                                                                              | `진행 예정`   |
 
 
 
