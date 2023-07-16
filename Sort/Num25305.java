a15f8373c806b2a438ead6964269eab5311c3338
TrendCatcher <karleo@kyonggi.ac.kr>
2023-07-16 21:05:27 +0900

    update [Sorting] Num25305

--- Sort/Num25305.java
+++ Sort/Num25305.java
@@ -4,23 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Num25305 {
     public static void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int [] arr = new int [n];
 
-        int [] arr = new int [N];
-
-        for(int i=0; i<N;i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        //StringTokenizer는 항상 사용되고 나서 재사용을 위해(arr[]요소를 읽기 위해) 리셋되거나 재 초기화 되어야 함
+        for(int i=0; i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
 
-        System.out.println(arr[k-1]);
+        System.out.println(arr[n-k]);
+
     }
+
     public static void main(String[]args) throws IOException{
         new Num25305().solution();
     }
