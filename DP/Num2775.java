039b49045fc6eeff5d93d170113cf1c8a0cb2835
TrendCatcher <karleo@kyonggi.ac.kr>
2023-08-12 19:14:24 +0900

    updated: [DP] Num2775 using StringBuilder

--- DP/Num2775.java
+++ DP/Num2775.java
@@ -9,27 +9,34 @@ package DP;
 *
 * [출력]
 * */
-import java.util.Scanner;
-public class Num2775 {
-    public static int solution(int k, int n){
-        int [][] residents = new int[k+1][n+1];
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ class Num2775 {
+     int [][] residents = new int[15][15];
 
-        for(int i=1;i<=n;i++){          // base case
+       void solution() throws IOException{
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int T = Integer.parseInt(br.readLine());
+         StringBuilder sb = new StringBuilder();
+
+        for(int i=1;i<=14;i++){          // base case
             residents[0][i] = i;        // 0층의 i호에는 i명이 산다. (initialization)
         }
-        for(int i =1; i<=k; i++){
-            for(int j=1;j<n;j++)
+        for(int i =1; i<=14; i++){
+            for(int j=1;j<=14;j++)
                 residents[i][j] = residents[i-1][j]+ residents[i][j-1];
         }
         // recurrence(1층 이상인 경우)
 
-        return residents[k][n];
+         for(int i=0;i<T;i++){
+             int k = Integer.parseInt(br.readLine());
+             int n = Integer.parseInt(br.readLine());
+             sb.append(residents[k][n]).append('\n');
+         }
+           System.out.println(sb);
     }
-    public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
-        int k= scan.nextInt();
-        int n = scan.nextInt();
-
-        new Num2775().solution(k,n);
+    public static void main(String[]args) throws IOException{
+        new Num2775().solution();
     }
 }
