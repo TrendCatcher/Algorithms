00ad0215d9e000277340397c8e0052677b99a798
TrendCatcher <karleo@kyonggi.ac.kr>
2023-09-15 16:32:22 +0900

    updated[implemented]: [BFS]refactor Num2606

--- Sort/Num10989.java
+++ Sort/Num10989.java
@@ -5,18 +5,18 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-class Num10989 {
+class Main{
     private static void solution() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
-        int cnt []= new int [1000001];      // 입력받은 수의 빈도수를 저장하는 배열
+
+        int cnt [] = new int [1000001];
 
         for(int i=0;i<N;i++){
-            cnt[Integer.parseInt(br.readLine())]++;     //입력받은 수의 빈도수 체크
+            cnt[Integer.parseInt(br.readLine())]++;
         }
-        for(int i=1;i<1000001;i++){         // cnt배열 안의 빈도수 만큼 안의 수 출력
+        for(int i=1;i<1000001;i++){
             while(cnt[i]>0){
                 sb.append(i).append('\n');
                 cnt[i]--;
@@ -25,10 +25,42 @@ class Num10989 {
         System.out.println(sb);
     }
 
-    public static void main(String[] args) throws IOException{
-        new Num10989().solution();
+    public static void main() throws IOException{
+        new Main().solution();
     }
 }
+
+
+
+
+//import java.io.IOException;
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//class Num10989 {
+//    private static void solution() throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int N = Integer.parseInt(br.readLine());
+//        int cnt []= new int [1000001];      // 입력받은 수의 빈도수를 저장하는 배열
+//
+//        for(int i=0;i<N;i++){
+//            cnt[Integer.parseInt(br.readLine())]++;     //입력받은 수의 빈도수 체크
+//        }
+//        for(int i=1;i<1000001;i++){         // cnt배열 안의 빈도수 만큼 안의 수 출력
+//            while(cnt[i]>0){
+//                sb.append(i).append('\n');
+//                cnt[i]--;
+//            }
+//        }
+//        System.out.println(sb);
+//    }
+//
+//    public static void main(String[] args) throws IOException{
+//        new Num10989().solution();
+//    }
+//}
 
 
 
