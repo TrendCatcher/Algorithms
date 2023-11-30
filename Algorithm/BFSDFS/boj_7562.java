fc990590fe23d3cc4523b56060892849e22ad8a7
TrendCatcher <karleo@kyonggi.ac.kr>
2023-11-30 22:05:09 +0900

    created: boj-7562, Move of Night

--- Algorithm/BFSDFS/boj_7562.java
+++ Algorithm/BFSDFS/boj_7562.java
@@ -1,2 +1,73 @@
-package Algorithm.BFSDFS;public class boj_7562 {
+package Algorithm.BFSDFS;
+//나이트의 이동
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_7562 {
+    static int I;
+
+    static int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int[] dy = {2, 1, -1, -2, -2, -1, 1, 2};
+    //탐색할 2차원 배열 or 그래프
+    static int[][] arr;
+    // 방문처리를 위한 boolean type
+    static boolean[][] visited;
+    //현재좌표, 이동후 좌표
+    static int x1,y1,x2, y2;
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            I = Integer.parseInt(br.readLine());
+            arr = new int[I][I];
+            visited = new boolean[I][I];
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            bfs();
+
+            sb.append(arr[x2][y2]).append("\n");
+        }
+        System.out.println(sb);
+    }
+    static void bfs(){
+        Queue<int[]> q = new LinkedList<>();
+        //1. queue에 시작점 넣고 방문처리
+        q.add(new int[]{x1,y1});
+        visited[x1][y1] =true;
+
+        while(!q.isEmpty()){
+            int now[] = q.poll();
+            int nx = now[0];
+            int ny = now[1];
+
+            for(int i=0; i<8; i++){
+                int ox = nx+dx[i];
+                int oy = ny+dy[i];
+                //말이 이동할 수 있는 위치에 한해 queue에 넣고 카운터+1후 방문처리
+                if(ox >=0 && oy >=0 && ox<I && oy<I){
+                    if(!visited[ox][oy]){
+                        q.add(new int[]{ox,oy});
+                        arr[ox][oy] = arr[nx][ny]+1;
+                        visited[ox][oy]= true;
+                    }
+                }
+            }
+        }
+
+    }
+
 }
--- DP/boj_1149.java
+++ DP/boj_1149.java
@@ -1,2 +1,53 @@
-package DP;public class boj_1149 {
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+//RGB거리
+public class boj_1149 {
+    static int [][] arr;
+    static int [][] dp; // dp[i][0]: i번째까지 칠한곳의 최솟값 (단, 한재는 R)
+
+    static int min;
+
+    public static void main(String[]args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N][3];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());   // i번째 집의 빨간색의 값
+            arr[i][1] = Integer.parseInt(st.nextToken());   // i번째 집의 초록색의 값
+            arr[i][2] = Integer.parseInt(st.nextToken());   // i번째 집의 파란색의 값
+
+        }
+        /*1. 테이블 정의*/
+        dp = new int [N][3];
+        /*3. 초기값 설정*/
+        dp[0][0]= arr[0][0];
+        dp[0][1]= arr[0][1];
+        dp[0][2]= arr[0][2];
+
+        /*2. 점화식 찾기*/
+        for(int i=1;i<N;i++){
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+            min = dp[i][0];
+            for(int j=1; j<=2; j++){
+                if(min > dp[i][j]) min = dp[i][j];
+            }
+        }
+        System.out.println(min);
+
+//    특정 칸에 왔을 때 그 칸까지의 최솟값이 있다고 가정, 현재의 최솟값(최소비용의 색깔)을 더하면
+//    특정 칸 까지의 최소비용 완성
+//    예시) dp[i][0] = min(dp[i-1][1],dp[i-1][2])+ arr[i][0];
+
+    }
+
 }
