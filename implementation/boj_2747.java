7d52ffdf7018c3a3b8b0ca02d5187521d518e50b
TrendCatcher <karleo@kyonggi.ac.kr>
2024-09-04 13:11:13 +0900

    [completed] implementation bronze 2 fib using bottom-up

--- implementation/boj_2747.java
+++ implementation/boj_2747.java
@@ -1,2 +1,26 @@
-package implementation;public class boj_2747 {
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2747 {
+    static int [] arr;
+
+
+    public static void main(String[]args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[n+1];
+        //base case
+        arr[0]=0;
+        arr[1]=1;
+        //상태전이 방정식
+        for(int i=2; i< arr.length;i++){
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        System.out.println(arr[n]);
+    }
 }
