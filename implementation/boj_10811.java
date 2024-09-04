b889b51c5a635141bf184ab334e3c8926d208596
TrendCatcher <karleo@kyonggi.ac.kr>
2024-09-04 12:54:13 +0900

    [reviewed] reverse implementation

--- implementation/boj_10811.java
+++ implementation/boj_10811.java
@@ -1,48 +1,41 @@
 package implementation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class boj_10811 {
-    static Integer[] arr;
-    public static void main(String[]args) throws IOException {
+class boj_10811{
+    static int a,b;
+    static int []arr;
+
+    //역순 구현부 (1<=i,j<N)
+    static void reverse(int []arr, int a, int b){
+        while(a<b){
+            int tmp = arr[a];
+            arr[a++] = arr[b];
+            arr[b--] = tmp;
+        }
+    }
+
+    public static void main(String[]args)throws IOException{    //입력부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        arr = new Integer[N];
-        //1부터 N번까지 배열 생성
+        arr = new int[N];    //배열 크기 선언
 
+        //arr 1~N으로 초기화
         for(int i=0; i<arr.length;i++){
-            arr[i] = i + 1;
+            arr[i] = i+1;
         }
-        //M번 만큼 범위 입력받기 + 입력받고 뒤집는 함수 reverse호출 (M번 수행)
-        for(int i=0;i<M;i++){
+
+        for(int i=0; i<M;i++){    // M의 개수만큼 역순으로 바꾸자하는 범위 입력 받기
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            reverse(arr ,a-1, b-1);
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            reverse(arr,a-1,b-1);    //역순 구현 함수 호출
         }
-
-//        for(int i=0; i< arr.length;i++){
-//            System.out.print(arr[i]+" ");
-//        }
-        //for - each 문
-        for(int num :arr){
-            System.out.print(num +" ");
+        for(int num: arr){    //[배열 출력부] for-each문을 통한 arr내 모든 배열을 공백단위로
+            System.out.print(num+" ");
         }
-    }
-
-     static void reverse(Integer []arr, int a, int b) {
-
-        while(a<b) {
-            int tmp = arr[b];
-            arr[b--] = arr[a];
-            arr[a++] = tmp;
-        }
-    }
-
-}
+    }//end of main
+}//end of class
