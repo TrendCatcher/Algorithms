4689ac932219ff0fcd6c87eb763f7605a6db5052
TrendCatcher <karleo@kyonggi.ac.kr>
2025-03-25 12:36:02 +0900

    [solved] sum of rests

--- implementation/boj_9506.java
+++ implementation/boj_9506.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class boj_9506 {
-    static ArrayList<Integer> nums = new ArrayList<>();
+
     static StringBuilder sb;
     public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,29 +15,31 @@ public class boj_9506 {
         while(true){
             int n = Integer.parseInt(br.readLine());
             //약수의 합을 구하는 로직, 2<n<100,000
-            //약수란? n보다 작은 모든 수로 나누었을때 나머지가 0
-            for(int i=1;i<=n/2;i++){    // 약수중 가장 큰수는 n/2를 넘지 않음:
-                if(n/i==0){
+
+            if(n==-1)   //-1이면 TC를 빠져나감
+                break;
+            ArrayList<Integer> nums = new ArrayList<>();
+            for(int i=1;i<n;i++){   //자기 자신을 제외한 약수의 합
+                if(n%i==0){   //약수란? n보다 작은 모든 수로 나누었을때 나머지가 0
                     //ArrayList에 등록
                     nums.add(i);
                 }
             }
-            //완전수인 경우 약수들의 합으로 출력
-            if(sumList(nums)==n){
-                sb.append(n).append(" = ");
-                for(int j:nums){
-                    sb.append(j).append(" + "); //약수들의 합으로 어떻게 출력하지..?
-                }
 
-                System.out.println(sb);
+            //완전수 아닌 경우
+            if(sumList(nums)!=n) {
+                sb.append(n + " is NOT perfect.\n");
+                continue;
             }else{
-                System.out.println(n + " is NOT perfect.");
+                sb.append(n).append(" = ");
+                for(int i=0; i<nums.size();i++){
+                    if(i>0) sb.append(" + ");
+                    sb.append(nums.get(i));
+                }
+                sb.append("\n");
             }
-
-            if(n==-1)   //-1이면 TC를 빠져나감
-                break;
         }
-
+        System.out.println(sb);
     }
     //(배열안의 수)모든 약수를 더하는 로직
     static int sumList(ArrayList<Integer> list){
