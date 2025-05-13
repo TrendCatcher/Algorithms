f36c41f3a6d2513780545842038c34ae95b9009a
TrendCatcher <karleo@kyonggi.ac.kr>
2025-05-13 12:11:29 +0900

    [complete] Catious with type of using round()

---
+++ DP/boj_23971.java
@@ -0,0 +1,25 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_23971 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());   //행
+        int W = Integer.parseInt(st.nextToken());   //열
+        int N = Integer.parseInt(st.nextToken());   //세로, N+1칸마다 하나씩 설치 가능
+        int M = Integer.parseInt(st.nextToken());   //가로, M+1칸마다
+
+
+        int row = (int)Math.ceil((double)H / (N + 1));
+        int col = (int)Math.ceil((double)W / (M + 1));
+
+        System.out.println(row * col);
+    }
+
+}
