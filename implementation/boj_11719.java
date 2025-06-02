3dc7958dc311987d8c72c54f2d2bf4a76580a877
TrendCatcher <karleo@kyonggi.ac.kr>
2025-06-02 13:28:55 +0900

    [review] print as it is using BufferedReader

--- implementation/boj_11719.java
+++ implementation/boj_11719.java
@@ -3,9 +3,9 @@ import java.io.*;
 
 public class boj_11719 {
     public static void main(String[]args)throws IOException{
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));){
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
             String line;
-            while((line=br.readLine())!=null){     //line을 입력받고 null이 아니면 그대로 출력
+            while((line = br.readLine())!=null){        //입력이 null이 아니면 line그대로 출력
                 System.out.println(line);
             }
         }catch(IOException e){
