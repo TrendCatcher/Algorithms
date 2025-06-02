e6d31644383fe1583c614b1797186f0c0b756ef6
TrendCatcher <karleo@kyonggi.ac.kr>
2025-06-02 13:48:57 +0900

    [review] Encoding through ROT 13

--- implementation/boj_11655.java
+++ implementation/boj_11655.java
@@ -2,21 +2,41 @@ package implementation;
 
 import java.io.*;
 class boj_11655{
-    static String[] words;
+//    static String[] words;
+//    static char[] arr;
+//    public static void main(String[]args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String line = br.readLine();
+//
+//        arr = line.toCharArray();
+//        StringBuilder sb = new StringBuilder();
+//        for(int j=0; j<arr.length;j++){
+//            if(arr[j]>='a' && arr[j]<='z'){
+//                arr[j] = (char)((arr[j]-'a'+13)%26+'a');
+//            }else if(arr[j]>='A'&& arr[j]<='Z'){
+//                arr[j] = (char)((arr[j]-'A'+13)%26+'A');
+//            }
+//            sb.append(arr[j]);
+//        }
+//        System.out.println(sb);
+//    }
     static char[] arr;
+
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
 
+        String line = br.readLine();
         arr = line.toCharArray();
+
         StringBuilder sb = new StringBuilder();
-        for(int j=0; j<arr.length;j++){
-            if(arr[j]>='a' && arr[j]<='z'){
-                arr[j] = (char)((arr[j]-'a'+13)%26+'a');
-            }else if(arr[j]>='A'&& arr[j]<='Z'){
-                arr[j] = (char)((arr[j]-'A'+13)%26+'A');
+
+        for(int i=0; i<arr.length;i++){     //ROT 13 Encoding
+            if(arr[i]>='a'&&arr[i]<='z'){
+                arr[i] = (char)((arr[i]-'a'+13)%26+'a');
+            }else if(arr[i]>='A'&&arr[i]<='Z'){
+                arr[i] = (char)((arr[i]-'A'+13)%26+'A');
             }
-            sb.append(arr[j]);
+            sb.append(arr[i]);
         }
         System.out.println(sb);
     }
