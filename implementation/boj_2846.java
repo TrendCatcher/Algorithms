3ca9df828b19f5803e3bded281f0b519b5a99a8e
TrendCatcher <karleo@kyonggi.ac.kr>
2025-06-10 11:56:24 +0900

    [solved] more clear code

--- implementation/boj_2846.java
+++ implementation/boj_2846.java
@@ -5,9 +5,7 @@ import java.util.*;
 
 class boj_2846{
     public static void main(String[]args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<Integer> set = new HashSet<>();
-        //최대 오르막길크기 구하기 = 끝- 시작
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//최대 오르막길크기 구하기 = 끝- 시작
         //계속 수열이 증가하면 다른 배열에 넣고 아니면 break;
 
         int n  =Integer.parseInt(br.readLine());
@@ -20,32 +18,16 @@ class boj_2846{
         }
 
         int max =0;
-        for(int i=1;i<arr.length;i++){
-            for(int j=i-1;j<i;j++){ //전의 요소와만 비교
-                if(arr[j]<arr[i]){  //뒤의 원소가 크다면 set.add
-                    //집합에 요소 추가
-                    set.add(arr[j]);
-                    set.add(arr[i]);
-                }else{
-                    break;      //아니면 빠져나옴
-                }
+        int start = arr[0];
+
+        for(int i=1;i<n;i++){
+            if(arr[i-1]<arr[i]){
+                max =Math.max(max,arr[i]-start);
+            }else{
+                start = arr[i];     //오르막 끝났으면 초기화
             }
-
-            List<Integer> list = new ArrayList<>();
-
-            for(int elements:set){
-                list.add(elements);
-            }
-            int[] elements = new int[list.size()];
-
-            for(int j=0;j<list.size();j++){
-                elements[i] = list.get(i);
-            }
-            Arrays.sort(elements);
-
-            max=Math.max(max,((elements[list.size()-1])-elements[0]));
         }   //end of for loop
 
-
+        System.out.println(max);
     }
 }
