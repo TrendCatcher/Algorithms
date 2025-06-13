ad4c67698cdf8b67d90b724d4258ec2b9423dfad
TrendCatcher <karleo@kyonggi.ac.kr>
2025-06-13 11:06:24 +0900

    [unsolved] wrong code

--- BFSDFS/boj_2178.java
+++ BFSDFS/boj_2178.java
@@ -1,70 +1,77 @@
 package BFSDFS;
+
 import java.io.*;
 import java.util.*;
+//구하는 값: 최소 칸수 -> BFS
 
 public class boj_2178 {
+    static int row;
+    static int column;
     static int[][] arr;
-    static StringTokenizer st;
-    static boolean[][] visited;
-    static int N;
-    static int M;
-    static int[] dx = {1,-1,0,0};
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+    static boolean[][] visited;
 
-    public static void main(String[]args)throws IOException{
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        row = Integer.parseInt(st.nextToken());
+        column = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][M];
-        visited = new boolean[N][M];
+        arr = new int[row][column];
+        visited = new boolean[row][column];
 
-        for(int i=0;i<N;i++){
+        for (int i = 0; i < row; i++) {
             char[] line = br.readLine().toCharArray();
-            for(int j=0; j<M;j++){
-                arr[i][j]=line[j]-'0';
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = line[j] - '0';
             }
         }
 
-        int result = Integer.MAX_VALUE;
+        int result = 0;
+        int distance = 1;   //distance 초기값은 1
 
-        for(int i=0; i<N;i++){
-            for(int j=0; j<M;j++){
-                if(arr[i][j]==1&&!visited[i][j]){
-                    result = Math.min(result,bfs(i,j));
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (arr[i][j] == 1 && !visited[i][j]) {   //갈수 있으면 탐색
+                    result = Math.min(result, bfs(i, j, distance));
                 }
             }
         }
         System.out.println(result);
-    }
-    static int bfs(int x, int y){
+    }//end of main
+
+    static int bfs(int x, int y, int distance) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y,1});
+        q.offer(new int[]{x, y, distance});
         visited[x][y] = true;
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {  //큐 빌때 까지
+
             int[] current = q.poll();
             int cx = current[0];
             int cy = current[1];
-            int distance = current[2];
-
-            if(cx==N-1&&cy==M-1){
-                return distance;
-            }
+            int dis = current[2];
 
             for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (!visited[nx][ny] && arr[nx][ny] == 1) {
-                        visited[nx][ny]=true;
-                        q.offer(new int[]{nx,ny,distance+1});
+                if (nx >= 0 && nx < row && ny >= 0 && ny < column) {
+                    if (arr[nx][ny] == 1 && !visited[nx][ny]) {
+                        if (nx == row - 1 && ny == column - 1) {    //목적지에 도달했으면 지나온 칸수 리턴
+                            return dis;
+                        } else { // 아직 목적지 탐색 안됬으면 recursion
+                            q.offer(new int[]{nx, ny, dis + 1});    //다시 큐에 넣고
+                            visited[nx][ny] = true; //방문처리
+                        }
                     }
                 }
-            }
+            }//end of for-loop
 
-        }//end of while
+        }
         return -1;
     }
-}
+}//end of class
--- implementation/boj_1009.java
+++ implementation/boj_1009.java
@@ -5,35 +5,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj_1009 {
+//public class boj_1009 {
+//    static StringTokenizer st;
+//
+//    public static void main(String[]args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int TC = Integer.parseInt(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        for(int i=0; i<TC;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            int result =1;
+//            for(int j=1; j<=b;j++){
+//                //a가 0일때는 0출력, 10의 배수일 때는 10출력
+//                if(a==0){
+//                    result = 0;
+//                    break;
+//                }else if(a%10==0){
+//                    result =10;
+//                    break;
+//                }
+//                result = (result * a) % 10;
+//            }
+//            sb.append(result).append("\n");
+//        }
+//        System.out.println(sb);
+//
+//
+//    }// end of main
+//}//end of class
+
+class boj_1009{
     static StringTokenizer st;
-
-    public static void main(String[]args) throws IOException {
+    static int result;
+    public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int TC = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
+
+        int TC = Integer.parseInt(br.readLine());
 
         for(int i=0; i<TC;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int result =1;
-            for(int j=1; j<=b;j++){
-                //a가 0일때는 0출력, 10의 배수일 때는 10출력
-                if(a==0){
-                    result = 0;
-                    break;
-                }else if(a%10==0){
-                    result =10;
-                    break;
+
+            for(int j=1; j<b; j++){
+                if(a%10 ==0){
+                    result = 10;
+                    sb.append(result).append("\n");
                 }
-                result = (result * a) % 10;
+                result = (1 * result) % 10;
+                sb.append(result).append("\n");
             }
-            sb.append(result).append("\n");
         }
         System.out.println(sb);
-
-
-    }// end of main
+    }//end of main
 }//end of class
