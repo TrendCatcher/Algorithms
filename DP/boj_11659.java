51d8e291708a43ceebaf282584c7c72303d225e3
TrendCatcher <karleo@kyonggi.ac.kr>
2025-06-17 14:19:41 +0900

    [review]  Again careful of intializing size of array

--- DP/boj_11659.java
+++ DP/boj_11659.java
@@ -2,38 +2,38 @@ package DP;
 
 import java.io.*;
 import java.util.*;
-
+//dp의 풀이법 >> memoization
 
 public class boj_11659 {
     static int[] prefix;
     static StringTokenizer st;
-    public static void main(String[]args)throws IOException{
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N,M;
+        int N, M;
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-
+        //1. 배열 정의 + 3.base case: prefix[i]는 i번째 까지의 합 > 이미 다 구해 놓는다.
+        prefix = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        /*[핵심] 구간합을 미리 구한다!!*/
-        prefix = new int[N+1];  //prefix[N] = N까지의 누적합
-        for(int i=1;i<=N;i++){
-            prefix[i] = prefix[i-1]+Integer.parseInt(st.nextToken());       //prefix[N]은 이전 원소 + 현재 받는 값
+        for (int i = 1; i <= N; i++) {  //prefix[1] = prefix[0] + 현재입력받는 원소
+            prefix[i] = prefix[i - 1] + Integer.parseInt(st.nextToken());
         }
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            int sum=0;
+
+        //2. 점화식 설정: 결과는 j번째까지의 합 - i-1번째 까지의 합이다.
+        for (int i = 0; i < M; i++) {       //M은 100000  O(M)
+            int result = 0;
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            sum = prefix[b] - prefix[a-1];
-            sb.append(sum).append("\n");
+            result = prefix[b] - prefix[a - 1];
+            sb.append(result).append("\n");
         }
         System.out.println(sb);
     }
-
 }
 
