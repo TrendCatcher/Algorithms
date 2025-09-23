a4b34842cec2c493b2ef9340e74c33d335439fe2
TrendCatcher <karleo@kyonggi.ac.kr>
2025-09-23 12:47:28 +0900

    [solved] <fixed> using two-pointer for swap logic  ,imple bronze 2

--- implementation/boj_10804.java
+++ implementation/boj_10804.java
@@ -7,9 +7,6 @@ class boj_10804{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[21];
-
-        int [] rev = new int[21];
-
         // 배열 1,2,3,,,20까지 초기화
         for(int i=1; i<21;i++){
             arr[i]=i;
@@ -19,19 +16,18 @@ class boj_10804{
             st = new StringTokenizer(br.readLine(), " ");
             int a=Integer.parseInt(st.nextToken());
             int b=Integer.parseInt(st.nextToken());
-            //a부터 b까지 배열을 뗴어와서 rev에 역순 대입후
-            for(int j=a;j<=b;j++){
-                rev[b-j+1] = arr[j];        //wrong line!!!
-            }
-            //역순 바뀐 구간을 다시 arr에 넣기
-            for(int j=a;j<=b;j++){
-                arr[j]= rev[j];
+            //a부터 b까지 배열을 뗴어와서 서로 swap
+
+            for(int j=0;j<=(b-a)/2;j++){    //구간을 절반만 순회
+                int tmp = arr[a+j]; //a+j구간의 왼쪽에서 j번째 원소
+                arr[a+j] = arr[b-j];//b-j구간의 오른쪽에서 j번쨰 원소
+                arr[b-j] = tmp;
             }
         }
         //출력
         StringBuilder sb = new StringBuilder();
         for(int i:arr){
-            sb.append(i).append(" ");
+            if(i!=0) sb.append(i).append(" ");
         }
         System.out.println(sb.toString().trim());
 
